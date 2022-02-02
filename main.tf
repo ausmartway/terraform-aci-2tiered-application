0f6a8f579ab724276bd0a46dd6621cb208c760e5
Yulei Liu <yulei.liu@gmail.com>
2022-02-02 12:53:35 +1100

    first draft

--- main.tf
+++ main.tf
@@ -1,1 +1,42 @@
 ##main.tf
+resource "aci_application_profile" "application_profile" {
+  tenant_dn = var.tenant_id
+  name      = var.application_name
+}
+
+resource "aci_application_epg" "tier1" {
+  name                   = "${var.application_name}-tier1"
+  application_profile_dn = aci_application_profile.application_profile.id
+  relation_fv_rs_bd      = var.bridge_domain
+  relation_fv_rs_prov    = [aci_contract.contract.id]
+}
+
+resource "aci_application_epg" "tier2" {
+  name                   = "${var.application_name}-tier2"
+  application_profile_dn = aci_application_profile.application_profile.id
+  relation_fv_rs_bd      = var.bridge_domain
+  relation_fv_rs_prov    = [aci_contract.contract.id]
+}
+
+#Contract Definitions
+resource "aci_contract" "contract" {
+  tenant_dn = var.tenant_id
+  name      = "${var.application_name}-contract"
+  scope     = "tenant"
+}
+
+# Subject Definitions
+
+resource "aci_contract_subject" "contractSubject" {
+  contract_dn                  = aci_contract.contract.id
+  name                         = "${var.application_name}-contractSubject"
+  relation_vz_rs_subj_filt_att = module.contractfilters.filterList
+}
+
+module "contractfilters" {
+  source  = "app.terraform.io/yulei/contractfilters/aci"
+  version = "0.0.7"
+  # insert required variables here
+  tenant_id = var.tenant_id
+  openports = var.openports
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,32 @@
 # Define your variables in here.
+
+variable "tenant_id" {
+  type        = string
+  description = "Tenant ID from ACI"
+}
+
+variable "bridge_domain" {
+  type        = string
+  description = "BridgeDomain ID from ACI"
+}
+
+variable "application_name" {
+  type        = string
+  description = "Name of the application"
+}
+
+variable "openports" {
+  default = {
+    # https = {
+    #   from_port = "443",
+    #   to_port   = "443"
+    #   protocol  = "tcp",
+    # },
+    # mysqldb = {
+    #   from_port = "3306",
+    #   to_port   = "3306",
+    #   protocol  = "tcp",
+    # }
+  }
+  description = "List of ports that should be open."
+}
